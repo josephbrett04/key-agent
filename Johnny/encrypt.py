9c25f8a93a28c2188ff089104eba9b5bee7fcdfd
John <hoangnghia2005@icloud.com>
2026-03-13 00:47:00 -0700

    added examples.py, also made encryptor class with encryption method

--- Johnny/encrypt.py
+++ Johnny/encrypt.py
@@ -1,2 +1,12 @@
-def from_string():
-    print("hi")
+import os
+from cryptography.hazmat.primitives.ciphers.aead import AESGCM
+
+class encryptor:
+    def __init__(self):
+        self.key = AESGCM.generate_key(bit_length=256)
+        self.worker = AESGCM(self.key)
+    
+    def encrypt_from_string(self, data):
+        nonce = os.urandom(12)
+        ciphertext = self.worker.encrypt(nonce, data, None)
+        return nonce, ciphertext
---
+++ Johnny/examples.py
@@ -0,0 +1,5 @@
+import encrypt
+
+encryptor = encrypt.encryptor()
+
+print(encryptor.encrypt_from_string(b"secret secret secret"))
